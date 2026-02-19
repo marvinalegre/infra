f152513945aee192f114796fe1163dcb6803f0e3
Marvin Alegre <marvinalegredev@gmail.com>
2026-02-19 11:56:54 +0800

    add setup scripts

---
+++ scripts/add-swap-space.sh
@@ -0,0 +1,12 @@
+SWAP_SIZE="1G"
+
+# Add Swap Space
+echo "Adding swap space..."
+sudo touch /swapfile
+sudo fallocate -l $SWAP_SIZE /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+# Make swap permanent
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
